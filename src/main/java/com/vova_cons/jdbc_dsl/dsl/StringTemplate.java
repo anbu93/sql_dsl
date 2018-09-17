c9c320266a9ab231817b15fa1836c0b6fe06839e
anbu <anbu23477@gmail.com>
2018-09-17 15:32:53 +0900

    v0.1.2: string template fix
    fixed string template - not using String.replaceAll (fucking regexp in
    this)

--- src/main/java/com/vova_cons/jdbc_dsl/dsl/StringTemplate.java
+++ src/main/java/com/vova_cons/jdbc_dsl/dsl/StringTemplate.java
@@ -1,22 +1,83 @@
 package com.vova_cons.jdbc_dsl.dsl;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  * Created by anbu on 17.09.18.
  **/
 public class StringTemplate {
-    private String template;
+    private List<Node> nodes;
 
     public StringTemplate(String template) {
-        this.template = template;
+        //TODO crutch method, this is fucking regexp in String.replaceAll metrhod!!!1
+        nodes = new LinkedList<>();
+        StringBuilder buffer = new StringBuilder();
+        for(char c : template.toCharArray()) {
+            if (c == '{') {
+                nodes.add(new Node(buffer.toString(), false));
+                buffer = new StringBuilder();
+            }
+            buffer.append(c);
+            if (c == '}') {
+                nodes.add(new Node(buffer.toString(), true));
+                buffer = new StringBuilder();
+            }
+        }
+        String last = buffer.toString();
+        if (!"".equals(last)) {
+            nodes.add(new Node(last, false));
+        }
+    }
+    public StringTemplate(List<Node> nodes) {
+        this.nodes = new LinkedList<>();
+        for(Node node : nodes) {
+            this.nodes.add(new Node(node.getValue(), node.isPattern()));
+        }
+    }
+    public void setNode(String key, String value) {
+        for(Node node : nodes) {
+            if (node.isPattern() && node.getValue().equals("{" + key + "}")) {
+                node.setValue(value);
+            }
+        }
     }
 
     public StringTemplate set(String key, Object value) {
-        return new StringTemplate(template.replaceAll("\\{"+key+"\\}", Objects.toString(value)));
+        StringTemplate newTemplate = new StringTemplate(nodes);
+        newTemplate.setNode(key, Objects.toString(value));
+        return newTemplate;
     }
 
     public String get() {
-        return template;
+        StringBuilder sb = new StringBuilder();
+        for(Node node : nodes) {
+            sb.append(node.getValue());
+        }
+        return sb.toString();
+    }
+
+    private static class Node {
+        private boolean isPattern;
+        private String value;
+
+        public Node(String value, boolean isPattern) {
+            this.value = value;
+            this.isPattern = isPattern;
+        }
+
+        public boolean isPattern() {
+            return isPattern;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
     }
 }
+
--- src/test/java/com/vova_cons/jdbc_dsl/db/SqlInsertQueryTest.java
+++ src/test/java/com/vova_cons/jdbc_dsl/db/SqlInsertQueryTest.java
@@ -2,6 +2,7 @@ package com.vova_cons.jdbc_dsl.db;
 
 import com.vova_cons.jdbc_dsl.dsl.SqlDsl;
 import com.vova_cons.jdbc_dsl.dsl.SqlTable;
+import com.vova_cons.jdbc_dsl.dsl.StringTemplate;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -26,6 +27,14 @@ public class SqlInsertQueryTest extends SqlDslTestParent {
                 .done((rs) -> new User(0, null), ((template, object) -> ""));
         String excepted = "INSERT INTO User(name) VALUES('{name}')";
         String actual = new SqlInsertQuery<>(null, table).getQuery();
+        assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void stringTemplateTest() {
+        String pattern = "INSERT INTO User(id, name) VALUES({id}, '{name}')";
+        String excepted = "INSERT INTO User(id, name) VALUES(5, 'vova_cons')";
+        String actual = new StringTemplate(pattern).set("id", 5).set("name", "vova_cons").get();
         assertEquals(excepted, actual);
     }
 
